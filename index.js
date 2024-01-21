d13c1c7f4919f39f7fc4f7b1faa3137dfebeec51
Nofisat Hassan <hassannofisat017@gmail.com>
2024-01-22 00:15:07 +0100

    added some changes

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-const items = [
+const products = [
   { name: "Concealer", price: 2500 },
   { name: "Foundation", price: 6000 },
   { name: "Eyeliner", price: 2000 },
   { name: "Brow Pencil", price: 1000 },
+  { name: "Mascara", price: 3500 },
   { name: "Blush", price: 2800 },
+  { name: "Lipliner", price: 500 },
+  { name: "Lip gloss", price: 3000 },
 ];
 
 const cart = [];
 
-function viewItems() {
+function viewProducts() {
   console.log("These are the available makeup products:");
-  items.forEach((item) => {
-    console.log(`${item.name} - ${item.price}`);
+  products.forEach((product) => {
+    console.log(`${product.name} - ${product.price}`);
   });
 }
 
 function addToCart(productName) {
-  const product = items.find(
+  const product = products.find(
     (item) => item.name.toLowerCase() === productName.toLowerCase()
   );
 
   if (product) {
     cart.push(product);
-    console.log("Added to cart!");
+    console.log("Product added to cart");
   } else {
-    console.log("Item not found");
+    console.log("Product not found");
   }
 }
 
 function viewCart() {
   console.log("Shopping Cart:");
-  cart.forEach((item) => {
-    console.log(`${item.name} - ${item.price}`);
+  cart.forEach((product) => {
+    console.log(`${product.name} - ${product.price}`);
   });
 }
 
 function totalCost() {
-  const total = cart.reduce((acc, item) => acc + item.price, 0);
-  console.log("The total cost is " + total);
+  const total = cart.reduce((acc, product) => acc + product.price, 0);
+  console.log("The total cost is " + total.toLocaleString());
 }
 
-viewItems();
+viewProducts();
 addToCart("Foundation");
 addToCart("Eyeliner");
 viewCart();
